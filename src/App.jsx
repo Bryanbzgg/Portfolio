243265790c5b2dc34e3224f60d10654baab01aca
Bryan <bryanratin8@gmail.com>
2025-03-18 17:04:09 -0300

    inserindo logo e imagens js,css,react

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,13 @@ function App() {
           <button onClick={() => window.open('https://github.com/yourprofile', '_blank')}>GitHub</button>
           <button onClick={() => window.open('https://twitter.com/yourprofile', '_blank')}>Twitter</button>
         </div>
+        <h2>Sobre Mim:</h2>
+        <p>Sou um estudante de Ensino Médio tecnico integrado com programação, buscando expandir meus conhecimentos na área. Apaixonado por e-sports e ciclismo,<br/> acredito que a dedicação e a estratégia de jogos eletrônicos se refletem em minha abordagem para resolver problemas de forma criativa e eficiente.<br/> Estou focado em concluir meus estudos e seguir carreira como programador, aplicando habilidades de raciocínio lógico, trabalho em equipe e constante aprendizado.</p>
+        <div className="tech-logos">
+          <img src="src/Components/img/logo-javascript.svg" alt="JavaScript Logo" className="tech-logo" />
+          <img src="src/Components/img/logo-css3.svg" alt="CSS Logo" className="tech-logo" />
+          <img src="src/Components/img/logo-react.svg" alt="React Logo" className="tech-logo" />
+        </div>
       </div>
       <Rodape />
     </>
